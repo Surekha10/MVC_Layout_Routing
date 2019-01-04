edfa09b30277a42a01438bd97fca051beb4963fa
Surekha10 <45581026+Surekha10@users.noreply.github.com>
2019-01-04 14:51:43 +0530

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Layout_Routing.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+        [Route("Pathfront")]
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+        [Route("Message/{msg:maxlength(10)?}")]
+        public ActionResult GetMessage(string msg="No Message")
+        {
+            ViewBag.msg = msg;
+            return View();
+        }
+    }
+}
